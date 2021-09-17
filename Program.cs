8296e56c55e46fb7808f3582f7915c3823b1f176
Savioli Pri <stobbeglaucya@gmail.com>
2021-09-17 18:28:14 -0300

    Adicionar resolção exercicio GrausCf

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace GrausCF
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            double c = 0;
+            double f = 0;
+
+            Console.Write("Entre com a temperatura na escala celsius: ");
+
+            c = double.Parse(Console.ReadLine());
+
+            f = (double)  c * 18 / 10 + 32;
+
+            Console.Write("Temperatura convertida em fahrenheit  é : " + f);
+            Console.ReadKey();
+        }
+    }
+}
